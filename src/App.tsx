9679204140d8d65ac576ec5e7135a59ea5c86aa2
Jo-x <vasilij.alexsandrovich@gmail.com>
2023-02-06 23:04:08 +0600

    set my template

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
+import styles from './App.module.scss';
+import { useAppDispatch, useAppSelector } from './redux/hooks/redux';
+import { showSlice } from './redux/reducers/ExampleSlice';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
+function App () {
+  const { plusOne, showHideWindow } = showSlice.actions;
+  const { counter, isShow } = useAppSelector(st => st.showSlice);
+  const dispatch = useAppDispatch();
+
+  const showReact = () => { dispatch(showHideWindow(!isShow)); };
+  const setCount = () => { dispatch(plusOne(counter + 1)); };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+
+      <button onClick={showReact} className={styles.Click}>star</button>
+      <img src={logo} className={styles.AppLogo} alt="logo" />
+
+      {
+        isShow
+          ? <><h1>Happy hacking!{counter}</h1> <button onClick={setCount}>count</button></>
+          : null
+      }
+
     </div>
   );
 }
